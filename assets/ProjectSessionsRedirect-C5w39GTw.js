2ddcc9dec84d2a15ec6c39d6ca1dd655e016e960
graehl <graehl@gmail.com>
2026-09-08 03:30:56 +0000

    Deploy: source 68aa86869 2026-09-08T03:30Z
    Contributing-model: 6-Astra

---
+++ assets/ProjectSessionsRedirect-C5w39GTw.js
@@ -0,0 +1,2 @@
+import{j as o,N as r}from"./main-BdYhMYPy.js";import{u as e}from"./useRemoteBasePath-CAfk3ED5.js";import"./react-runtime-D-UPQ57m.js";import"./RemoteConnectionContext-BKQfrT7i.js";import"./client-CASuVkF1.js";import"./events-Dqcw9CxN.js";import"./activityBus-Ff40c-YR.js";import"./sourceIdentity-COsjAfmX.js";import"./relay-protocol-JPg_jCN3.js";import"./remoteRoutePaths-BRtnNHim.js";function f(){const t=e();return o.jsx(r,{to:`${t}/sessions`,replace:!0})}export{f as ProjectSessionsRedirect};
+//# sourceMappingURL=ProjectSessionsRedirect-C5w39GTw.js.map
---
+++ assets/useRemoteBasePath-CAfk3ED5.js
@@ -0,0 +1,2 @@
+import{a as t}from"./RemoteConnectionContext-BKQfrT7i.js";import{a as o}from"./remoteRoutePaths-BRtnNHim.js";function n(){const e=t(),a=e==null?void 0:e.currentRelayUsername;return a?o(a):""}export{n as u};
+//# sourceMappingURL=useRemoteBasePath-CAfk3ED5.js.map
